cbc65a256c5c3e09d6a5a75d8f7d584fb68d7eb7
winaung678 <winaung.678@gmail.com>
2026-06-15 20:54:13 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'survey-pro-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './leaflet.css',
+  './leaflet.js',
+  './formulas.js',
+  './globals.js',
+  './map_engine.js',
+  './app_conversions.js',
+  './app_survey_tools.js',
+  './app_camera.js',
+  './app_logic.js',
+  './egm2008_1min.bin',
+  './egm96_global.bin',
+  './icon-192.png',
+  './icon-512.png',
+  'https://unpkg.com/dxf-parser/dist/dxf-parser.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache ထဲမှာရှိရင် အဲ့ဒါကို ပေးမယ် (Offline အလုပ်လုပ်မယ်)
+        if (response) {
+          return response;
+        }
+        // မရှိရင် အင်တာနက်ကနေ ဆွဲမယ်
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); // Cache အဟောင်းတွေ ဖျက်မယ်
+          }
+        })
+      );
+    })
+  );
+});
